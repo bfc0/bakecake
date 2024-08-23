e23420dc84734674c89ca465b6950b1806285c76
bfc0 <bfcheg@gmail.com>
2024-08-23 14:45:02 +0300

    Moved Admin models

--- cakes/admin.py
+++ cakes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (CatalogueCake, CustomCake, Level,
+                     Shape, Topping, Berry, Decoration, Event,)
 
-# Register your models here.
+admin.site.register(CatalogueCake)
+admin.site.register(CustomCake)
+admin.site.register(Level)
+admin.site.register(Shape)
+admin.site.register(Topping)
+admin.site.register(Berry)
+admin.site.register(Decoration)
+admin.site.register(Event)
