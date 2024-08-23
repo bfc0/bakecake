674510f7343526479c002e5fa888289eb9c2fcae
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-08-23 17:03:57 +0500

    change admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from cakes.models import Visit, CartAddition, Order
+
+
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+    list_display = ('user', 'session_key', 'timestamp')
+
+
+@admin.register(CartAddition)
+class CartAdditionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product_name', 'timestamp')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'total_amount', 'timestamp')
+
+    def total_revenue(self):
+        return sum(order.total_amount for order in Order.objects.all())
+
+    total_revenue.short_description = 'Total Revenue'
